45b4cdfb3bef5db2176cf6cd57239b1960b51dcc
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2024-07-16 10:30:34 +0200

    Add the app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# password-app
+# Generated Password application
+
+To run the app: `node server.js`
+
+## With Clever Cloud
+
+```
+clever create --org ORGA_NAME --type node --github jacquesbh/password-app password-app
+clever scale -a password-app --flavor pico
+```
+## License
+
+This very small project is under MIT License.
---
+++ server.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const { exec } = require('child_process');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        exec('pwgen -n 30 -y -1', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(`Error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(`Stderr: ${stderr}`);
+                return;
+            }
+            const password = stdout.trim();
+            const htmlContent = `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Generated Password</title>
+                </head>
+                <body>
+                    <pre id="password">${password}</pre>
+                    <script>
+                        function selectText() {
+                            const password = document.getElementById('password');
+                            const range = document.createRange();
+                            range.selectNode(password);
+                            window.getSelection().removeAllRanges();
+                            window.getSelection().addRange(range);
+                        }
+                        selectText()
+                    </script>
+                </body>
+                </html>
+            `;
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(htmlContent);
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
